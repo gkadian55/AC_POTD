9071794ee1ce549599961870b96d8cd7f8964ab2
Gaurav <gkadian55@gmail.com>
2025-12-16 02:42:38 +0530

    Solved the second question correctly

--- day_14/AddModulo10.java
+++ day_14/AddModulo10.java
@@ -7,16 +7,52 @@ public class AddModulo10 {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         StringBuilder sb = new StringBuilder("");
+
         while (t > 0) {
             int n = sc.nextInt();
-            long[] array = new long[n];
+            int[] array = new int[n];
             for (int i = 0; i < n; i++) {
-                long a = sc.nextLong();
-                if (a % 10 == 5) array[i] = (a + 5) % 20;
-                else array[i] = a % 20;
+                int a = sc.nextInt();
+                if (a % 10 == 0 || a % 10 == 5) { //fixed elements
+                    array[i] = a + (a % 10);
+                }
+                else {
+                    while (a % 10 != 2) { //normalizing cyclic elements
+                        a += (a % 10);
+                    }
+                    array[i] = a % 20;
+                }
             }
+
+            boolean oneZero = false, oneRest = false;
+            for (int a : array) {
+                if (a % 10 == 0) oneZero = true;
+                else oneRest = true;
+            }
+
             boolean holds = true;
-            
+
+            if (oneZero && oneRest) {
+                holds = false;
+            } 
+            else if (oneRest) {
+                int key = array[0];
+                for (int i = 1; i < n; i++) {
+                    if (array[i] != key) {
+                        holds = false;
+                        break;
+                    }
+                }
+            } 
+            else {
+                int key = array[0];
+                for (int i = 1; i < n; i++) {
+                    if (array[i] != key) {
+                        holds = false;
+                        break;
+                    }
+                }
+            }
             if (holds) sb.append("YES\n");
             else sb.append("NO\n");
             t--;

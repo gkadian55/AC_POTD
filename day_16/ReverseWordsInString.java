d4a52456b4aa0fd0f5ea9a91d940a46b8f74c1dd
Gaurav <gkadian55@gmail.com>
2025-12-18 02:01:46 +0530

    Updated the code for Ques.2

--- day_16/ReverseWordsInString.java
+++ day_16/ReverseWordsInString.java
@@ -2,18 +2,26 @@ package day_16;
 
 import java.util.*;
 
-public class ReverseWordsInString {
-    public static void main() {
-        String s = "the sky is blue";
+class Solution {
+    public String reverseWords(String s) {
+        s = s + " ";
         Stack<String> words = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             String word = "";
-            while (s.charAt(i) != ' ') {
-                word += Character.toString(s.charAt(i));
+            while (!(s.substring(i, i + 1).equals(" "))) {
+                word += s.substring(i, i + 1);
                 i++;
             }
+            if (word.equals("")) continue;
             words.push(word);
         }
-        System.out.println(words);
+        StringBuilder sb = new StringBuilder("");
+        int n = words.size();
+        for (int i = 0; i < n; i++) {
+            sb.append(words.pop());
+            if (i != n - 1) sb.append(" ");
+        }
+        String reverse = sb.toString();
+        return reverse;
     }
 }

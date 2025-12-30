cd2dfafc95597599fcc6d4b6ed2dda1a34dc9cd3
Gaurav <gkadian55@gmail.com>
2025-12-30 22:22:35 +0530

    Implemented Queue using Array

--- day_24/QueueUsingArray.java
+++ day_24/QueueUsingArray.java
@@ -1,5 +1,53 @@
 package day_24;
 
-public class QueueUsingArray {
+class myQueue {
+    int[] queue;
+    int size;
+    int front;
+    int rear;
+    int count;
     
+    public myQueue(int n) {
+        queue = new int[n];
+        size = n;
+        front = 0;
+        rear = -1;
+        count = 0;
+    }
+
+    public boolean isEmpty() {
+        if (count == 0) return true;
+        return false;
+    }
+
+    public boolean isFull() {
+        if (size == count) return true;
+        return false;
+    }
+
+    public void enqueue(int x) {
+        if (!isFull()) {
+            rear = (rear + 1) % size;
+            queue[rear] = x;
+            count++;
+        }
+    }
+
+    public void dequeue() {
+        if (!isEmpty()) {
+            front = (front + 1) % size;
+            count--;
+        }
+    }
+
+    public int getFront() {
+        if (!isEmpty()) return queue[front];
+        return -1;
+    }
+
+    public int getRear() {
+        if (!isEmpty()) return queue[rear];
+        return -1;
+    }
 }
+

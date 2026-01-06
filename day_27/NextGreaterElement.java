7f24bae0fffdec5532a5e01c38357c609d7b8d34
Gaurav <gkadian55@gmail.com>
2026-01-06 21:40:51 +0530

    Solved Next Greater Element using Monotonic Stack

--- day_27/NextGreaterElement.java
+++ day_27/NextGreaterElement.java
@@ -1,4 +1,28 @@
 package day_27;
 
-public class NextGreaterElement {
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        int n = arr.length;
+        for (int i = 0; i < n; i++) result.add(0);
+        for (int i = n-1; i >= 0; i--) {
+            int j = i;
+            while (j == i) {
+                if (stack.isEmpty()) {
+                    result.set(i, -1);
+                    j--;
+                }
+                else if (stack.peek() > arr[i]) {
+                    result.set(i, stack.peek());
+                    j--;
+                }
+                else stack.pop();
+            }
+            stack.push(arr[i]);
+        }
+        return result;
+    }
 }

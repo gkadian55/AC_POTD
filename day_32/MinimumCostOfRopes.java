2cba59864e78bb2b8b9ee853cd9ce7caa9256660
Gaurav <gkadian55@gmail.com>
2026-01-11 00:51:57 +0530

    Solved POTD #32 questions using PriorityQueue

--- day_32/MinimumCostOfRopes.java
+++ day_32/MinimumCostOfRopes.java
@@ -1,5 +1,18 @@
 package day_32;
 
-public class MinimumCostOfRopes {
-    
+import java.util.*;
+
+class Solution {
+    public static int minCost(int[] arr) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int a : arr) pq.add(a);
+        int cost = 0;
+        while (pq.size() != 1) {
+            int ropeOne = pq.poll();
+            int ropeTwo = pq.poll();
+            pq.add(ropeOne + ropeTwo);
+            cost += ropeOne + ropeTwo;
+        }
+        return cost;
+    }
 }
--- day_32/TakeGiftsFromRichestPile.java
+++ day_32/TakeGiftsFromRichestPile.java
@@ -4,13 +4,11 @@ import java.util.*;
 
 class Solution {
     public long pickGifts(int[] gifts, int k) {
-        int n = gifts.length - 1;
-        while (k-- > 0) {
-            Arrays.sort(gifts);
-            gifts[n] = (int) Math.sqrt(gifts[n]);
-        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int a : gifts) pq.add(a);
+        while (k-- > 0) pq.add((int) Math.sqrt(pq.poll()));
         long sum = 0;
-        for (int a : gifts) sum += a;
+        for (int a : pq) sum += a;
         return sum;
     }
 }

64fcd8c204ee15ad053b5f1e8d4fd932e1ff5c75
Gaurav <gkadian55@gmail.com>
2026-01-16 20:13:12 +0530

    Solved the 2nd question of POTD #33

--- day_33/GameWithString.java
+++ day_33/GameWithString.java
@@ -1,5 +1,23 @@
 package day_33;
 
-public class GameWithString {
-    
+import java.util.*;
+
+class Solution {
+    public int minValue(String s, int k) {
+        Map<Character, Integer> freq = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
+            pq.add(entry.getValue());
+        }
+        while (k-- > 0) {
+            pq.add(pq.poll() - 1);
+        }
+        int sum = 0;
+        while (!pq.isEmpty()) sum += pq.peek() * pq.poll();
+        return sum;
+    }
 }

4f9509489f4bd09825e1e0a64668b62b7fd3516b
Gaurav <gkadian55@gmail.com>
2026-01-19 19:35:10 +0530

    Solved the 2nd question of POTD #26

--- day_26/FirstNonRepeatingCharacterInStream.java
+++ day_26/FirstNonRepeatingCharacterInStream.java
@@ -1,5 +1,22 @@
 package day_26;
 
-public class FirstNonRepeatingCharacterInStream {
-    
+import java.util.*;
+
+class Solution {
+    public String firstNonRepeating(String s) {
+        StringBuilder sb = new StringBuilder();
+        int[] frequency = new int[26];
+        ArrayDeque<Character> queue = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            frequency[ch - 'a']++;
+            if (frequency[ch - 'a'] == 1) queue.add(ch);
+            while (!queue.isEmpty() && frequency[queue.peek() - 'a'] > 1) {
+                queue.poll();
+            }
+            if (queue.isEmpty()) sb.append('#');
+            else sb.append(queue.peek());
+        }
+        return sb.toString();
+    }
 }

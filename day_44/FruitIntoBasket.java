2160c7e8589f0975432bc839788bd6e08c14f3e4
Gaurav <gkadian55@gmail.com>
2026-01-22 22:04:12 +0530

    Solved the 2nd question of POTD #44

---
+++ day_44/FruitIntoBasket.java
@@ -0,0 +1,22 @@
+package day_44;
+
+import java.util.*;
+
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int n = fruits.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        int i = 0;
+        int max = 0;
+        for (int j = 0; j < n; j++) {
+            map.put(fruits[j], map.getOrDefault(fruits[j], 0) + 1);
+            while (map.size() > 2) {
+                map.put(fruits[i], map.get(fruits[i]) - 1);
+                if (map.get(fruits[i]) == 0) map.remove(fruits[i]);
+                i++;
+            }
+            max = Math.max(max, j - i + 1);
+        }
+        return max;
+    }
+}

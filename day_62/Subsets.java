6caa2035f8f22d3b63b7a21b42106ec7d61c8159
Gaurav <gkadian55@gmail.com>
2026-03-01 00:52:38 +0530

    Used recursive approach to solve the questions

--- day_62/Subsets.java
+++ day_62/Subsets.java
@@ -3,18 +3,20 @@ package day_62;
 import java.util.*;
 
 class Solution {
+    public void powerSet(int[] nums, List<Integer> subset, List<List<Integer>> answer, int i) {
+        if (i == nums.length) {
+            answer.add(new ArrayList<>(subset));
+            return;
+        }
+        subset.add(nums[i]);
+        powerSet(nums, subset, answer, i+1);
+        subset.remove(subset.size()-1);
+        powerSet(nums, subset, answer, i+1);
+    }
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
-        answer.add(new ArrayList<>());
-        for (int i = 0; i < nums.length; i++) {
-            int value = nums[i];
-            int length = answer.size();
-            for (int j = 0; j < length; j++) {
-                List<Integer> element = new ArrayList<>(answer.get(j));
-                element.add(value);
-                answer.add(element);
-            }
-        }
+        List<Integer> subset = new ArrayList<>();
+        powerSet(nums, subset, answer, 0);
         return answer;
     }
 }
--- day_62/SumOfAllSubsetXORTotals.java
+++ day_62/SumOfAllSubsetXORTotals.java
@@ -1,33 +1,17 @@
 package day_62;
 
-import java.util.*;
-
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> answer = new ArrayList<>();
-        answer.add(new ArrayList<>());
-        for (int i = 0; i < nums.length; i++) {
-            int value = nums[i];
-            int length = answer.size();
-            for (int j = 0; j < length; j++) {
-                List<Integer> element = new ArrayList<>(answer.get(j));
-                element.add(value);
-                answer.add(element);
-            }
+    public int powerSetXOR(int[] nums, int i, int xor) {
+        if (i == nums.length) {
+            return xor;
         }
-        return answer;
+        xor = xor ^ nums[i];
+        int include = powerSetXOR(nums, i+1, xor);
+        xor = xor ^ nums[i];
+        int exclude = powerSetXOR(nums, i+1, xor);
+        return include + exclude;
     }
     public int subsetXORSum(int[] nums) {
-        List<List<Integer>> subset = subsets(nums);
-        int ans = 0;
-        for (int i = 0; i < subset.size(); i++) {
-            List<Integer> sub = subset.get(i);
-            int xor = 0;
-            for (int j = 0; j < sub.size(); j++) {
-                xor ^= sub.get(j);
-            }
-            ans += xor;
-        }
-        return ans;
+        return powerSetXOR(nums, 0, 0);
     }
 }

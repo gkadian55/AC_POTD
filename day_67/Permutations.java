1f7a4e4335ab64789a793392498c884b1fac896b
Gaurav <gkadian55@gmail.com>
2026-03-04 22:01:21 +0530

    Implemented Permutations using Recursion

---
+++ day_67/Permutations.java
@@ -0,0 +1,28 @@
+package day_67;
+
+import java.util.*;
+
+class Solution {
+    public void generate(int[] nums, boolean[] marked, List<Integer> perm, List<List<Integer>> answer) {
+        if (perm.size() == nums.length) {
+            answer.add(new ArrayList<>(perm));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!marked[i]) {
+                perm.add(nums[i]);
+                marked[i] = true;
+                generate(nums, marked, perm, answer);
+                marked[i] = false;
+                perm.remove(perm.size()-1);
+            }
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        boolean[] marked = new boolean[nums.length];
+        List<List<Integer>> answer = new ArrayList<>();
+        List<Integer> perm = new ArrayList<>();
+        generate(nums, marked, perm, answer);
+        return answer;
+    }
+}
